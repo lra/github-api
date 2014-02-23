0fcb5c1a9fac89bfce5c17e70fbfd1a94fd212ba
Laurent Raufaste <analogue@glop.org>
2014-02-22 20:20:21 -0500

    Initial setup file to register the package name

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Setup file to automate the install of github-api in a Python environment"""
+import distutils.core
+
+
+distutils.core.setup(
+    name='github-api',
+    version='0.1',
+    author='Laurent Raufaste',
+    author_email='analogue@glop.org',
+    url='https://github.com/lra/github-api',
+)
